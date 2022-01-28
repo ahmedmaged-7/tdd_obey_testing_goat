11428362c946a2136c22e5597e3f3eae787d52c4
Ahmed Maged <ahmedmaged7717@gmail.com>
2022-01-28 23:44:16 +0200

    made the functional test not affect db

---
+++ superlists/functional test/test_ch6.py
@@ -0,0 +1,61 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from django.test import LiveServerTestCase
+
+class NewVisitorTest(LiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def check_row_exist(self,table_iD,row_text):
+         table = self.browser.find_element_by_id(table_iD)
+         rows = table.find_elements_by_tag_name('tr')
+         self.assertIn(row_text, [row.text for row in rows])
+        
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get(self.live_server_url)#it is live not life
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('the_to_do_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        inputbox = self.browser.find_element_by_id('the_to_do_item')
+
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list table
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        inputbox = self.browser.find_element_by_id('the_to_do_item')
+
+
+        inputbox.send_keys('use peacok feathers to make a fly')        
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        
+        self.check_row_exist("id_new_table","1: Buy peacock feathers")
+        self.check_row_exist("id_new_table","2: use peacok feathers to make a fly")
+       
+       
+
+
+
+
